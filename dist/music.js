c2003dca1a1e946954176da30794ba9eb8293d9d
whd98 <1091669306@qq.com>
2022-09-30 12:22:49 +0800

    Site updated: 2022-09-30 12:22:47

---
+++ dist/music.js
@@ -0,0 +1,13 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+	autoplay: true, //自动播放
+    audio: [
+	{
+        name: '忏悔录',
+        artist: 'KKECHO/那奇沃夫/DR1P',
+        url: '//music.163.com/outchain/player?type=2&id=1970396862&auto=1&height=66',
+        cover: '',
+    }, 
+	]
+});
